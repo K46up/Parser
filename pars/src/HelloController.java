2cddebe0abb4dbd56ad1468e7b010c561a5e400f
Student <vasiltsova-natulya@mail.ru>
2022-11-08 11:02:59 +0300

    something appeared

---
+++ pars/src/HelloController.java
@@ -0,0 +1,208 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class HelloController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField id_Search;
+
+    @FXML
+    private Label id_city;
+
+    @FXML
+    private Label id_date1;
+
+    @FXML
+    private Label id_date2;
+
+    @FXML
+    private Label id_date3;
+
+    @FXML
+    private Label id_date4;
+
+    @FXML
+    private Label id_date5;
+
+    @FXML
+    private Label id_date6;
+
+    @FXML
+    private Label id_date7;
+
+    @FXML
+    private Label id_date8;
+
+    @FXML
+    private ImageView id_dayImage1;
+
+    @FXML
+    private ImageView id_dayImage2;
+
+    @FXML
+    private ImageView id_dayImage3;
+
+    @FXML
+    private ImageView id_dayImage4;
+
+    @FXML
+    private ImageView id_dayImage5;
+
+    @FXML
+    private ImageView id_dayImage6;
+
+    @FXML
+    private ImageView id_dayImage7;
+
+    @FXML
+    private ImageView id_dayImage8;
+
+    @FXML
+    private Label id_dayWeek1;
+
+    @FXML
+    private Label id_dayWeek2;
+
+    @FXML
+    private Label id_dayWeek3;
+
+    @FXML
+    private Label id_dayWeek4;
+
+    @FXML
+    private Label id_dayWeek5;
+
+    @FXML
+    private Label id_dayWeek6;
+
+    @FXML
+    private Label id_dayWeek7;
+
+    @FXML
+    private Label id_dayWeek8;
+
+    @FXML
+    private Label id_degreesEvening1;
+
+    @FXML
+    private Label id_degreesEvening2;
+
+    @FXML
+    private Label id_degreesEvening3;
+
+    @FXML
+    private Label id_degreesEvening4;
+
+    @FXML
+    private Label id_degreesEvening5;
+
+    @FXML
+    private Label id_degreesEvening6;
+
+    @FXML
+    private Label id_degreesEvening7;
+
+    @FXML
+    private Label id_degreesEvening8;
+
+    @FXML
+    private Label id_degreesMorning1;
+
+    @FXML
+    private Label id_degreesMorning2;
+
+    @FXML
+    private Label id_degreesMorning3;
+
+    @FXML
+    private Label id_degreesMorning4;
+
+    @FXML
+    private Label id_degreesMorning5;
+
+    @FXML
+    private Label id_degreesMorning6;
+
+    @FXML
+    private Label id_degreesMorning7;
+
+    @FXML
+    private Label id_degreesMorning8;
+
+    @FXML
+    private Label id_eighteen;
+
+    @FXML
+    private ImageView id_eighteenImage;
+
+    @FXML
+    private Label id_fifteen;
+
+    @FXML
+    private ImageView id_fifteenImage;
+
+    @FXML
+    private Label id_humidity;
+
+    @FXML
+    private Label id_nine;
+
+    @FXML
+    private ImageView id_nineImage;
+
+    @FXML
+    private Label id_pressure;
+
+    @FXML
+    private Label id_six;
+
+    @FXML
+    private ImageView id_sixImage;
+
+    @FXML
+    private Label id_three;
+
+    @FXML
+    private ImageView id_threeImage;
+
+    @FXML
+    private Label id_time;
+
+    @FXML
+    private Label id_twelve;
+
+    @FXML
+    private ImageView id_twelveImage;
+
+    @FXML
+    private Label id_twentyOne;
+
+    @FXML
+    private ImageView id_twentyOneImage;
+
+    @FXML
+    private Label id_wind;
+
+    @FXML
+    private Label id_zero;
+
+    @FXML
+    private ImageView id_zeroImage;
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
